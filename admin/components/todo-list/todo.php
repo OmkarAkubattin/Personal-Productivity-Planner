ea92aca23130aa4855ae6b3541a2440b8964b742
OmkarAkubattin <akubattino11@gmail.com>
2023-12-17 19:54:57 +0530

    master

--- admin/components/todo-list/todo.php
+++ admin/components/todo-list/todo.php
@@ -221,7 +221,14 @@
                                 <label for="exampleFormControlTextarea1" required>Task Description</label>
                                 <textarea class="form-control" name="disc" rows="3">'.$row['disc'].'</textarea>
                                 <label for="exampleFormControlTextarea1">Task Tags</label>
-                                <input class="form-control" value="'.$row['tag'].'" name="tag" rows="1"></input>
+                                <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">';
+                                $result1=sql_query("SELECT DISTINCT tag FROM `todo` WHERE tag != '' and `fk_user`='$id'");
+                                if (mysqli_num_rows($result1) >0) {
+                                while($row1 = mysqli_fetch_assoc($result1)){
+                                if($row['tag']==$row1['tag']) echo '<option value="'.$row1['tag'].'">'.$row1['tag'].'</option>';
+                                else echo '<option value="'.$row1['tag'].'">'.$row1['tag'].'</option>';}}
+                                echo'</select>
+                            </div>
                             </div>
                         </form>';}}
                     }
@@ -241,17 +248,12 @@
                             <label for="exampleFormControlTextarea1" required>Task Description</label>
                             <textarea class="form-control" name="disc" rows="3"></textarea>
                             <label for="exampleFormControlTextarea1">Task Tags</label>
-                            <select class="form-select" aria-label="Default select example" onchange="this.nextElementSibling.value=this.value">
-                            <option value=""></option>
-                            <option value="Personal">Personal</option>
-                            <option value="Work">Work</option>
-                            <option value="Other">Other</option>';
-                            $result1=sql_query("SELECT DISTINCT tag FROM `todo` WHERE tag != '' and `fk_user`='$id'");
-                            if (mysqli_num_rows($result1) >0) {
-                            while($row1 = mysqli_fetch_assoc($result1)){
-                            echo '<option value="'.$row1['tag'].'">'.$row1['tag'].'</option>';}}
-                            echo'</select>
-                            </div>
+                            <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">';
+                                $result1=sql_query("SELECT DISTINCT tag FROM `todo` WHERE tag != '' and `fk_user`='$id'");
+                                if (mysqli_num_rows($result1) >0) {
+                                while($row1 = mysqli_fetch_assoc($result1)){
+                                echo '<option value="'.$row1['tag'].'">'.$row1['tag'].'</option>';}}
+                                echo'</select>
                         </div>
                         
                         </div>
