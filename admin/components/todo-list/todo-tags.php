ad4385744d3809e9424c756665b8952741b50f26
OmkarAkubattin <akubattino11@gmail.com>
2023-12-20 23:54:17 +0530

    master

--- admin/components/todo-list/todo-tags.php
+++ admin/components/todo-list/todo-tags.php
@@ -35,6 +35,7 @@
     <!-- Custom fonts for this template -->
     <link href="/Personal-Productivity-Planner/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet"
         type="text/css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -322,6 +323,7 @@
         <!-- End of Page Wrapper -->
 
         <!-- Scroll to Top Button-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>                                        
         <a class="scroll-to-top rounded" href="/Personal-Productivity-Planner/admin/#page-top">
             <i class="fas fa-angle-up"></i>
         </a>
